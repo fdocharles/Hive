1240ff80634697df4f697732605c9b0af800a198
KannuSingh <karandeepsinghcaj@outlook.com>
2022-04-16 15:13:40 -0400

    Added Basic project structure and navigation flow
    Added Basic project structure
    -Fragment for SplashScreen
    -Fragement For Login, Registration, Customer Home, Service Provider Home
    Added navigation flow for these

--- app/src/main/java/com/example/hive/MainActivity.java
+++ app/src/main/java/com/example/hive/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.hive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//This activity should check user is logged in or not ,
+//User interface of the activity should show loading when checking user loggedin details
+//User interface should have option for selecting service provider and customer button.
 public class MainActivity extends AppCompatActivity {
 
     @Override
