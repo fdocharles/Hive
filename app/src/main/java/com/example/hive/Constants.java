747daee15f4046eef66765d9a45272b15287ed45
fdocharles <fdocharles@gmail.com>
2022-04-20 20:34:06 -0400

    Linked splash screen with user type select screen and assigned click listener to the get hired and hire buttons

--- app/src/main/java/com/example/hive/Constants.java
+++ app/src/main/java/com/example/hive/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String SERVICE_PLUMBER = "PLUMBER";
     public static final String SERVICE_SECURITY = "SECURITY";
     public static final String SERVICE_WAITER = "WAITER";
+    public static final String KEY_SELECTED_SERVICE = "SELECTED_SERVICE";
 }
--- app/src/main/java/com/example/hive/CustomerServicesFragment.java
+++ app/src/main/java/com/example/hive/CustomerServicesFragment.java
@@ -3,6 +3,7 @@ package com.example.hive;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ public class CustomerServicesFragment extends Fragment {
 
 
     LinearLayout construction,cook,doctor,investigator,lawyer,plumber,security,waiter;
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +24,7 @@ public class CustomerServicesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_customer_services, container, false);
+        view = inflater.inflate(R.layout.fragment_customer_services, container, false);
 
         construction = (LinearLayout) view.findViewById(R.id.construction_layout);
         cook = (LinearLayout) view.findViewById(R.id.cook_layout);
@@ -58,6 +60,13 @@ public class CustomerServicesFragment extends Fragment {
 
     private void redirectToServiceProviderList(String data)
     {
+        Bundle bundle = new Bundle();
+        if(!data.isEmpty())
+        {
 
+            bundle.putString(Constants.KEY_SELECTED_SERVICE, data);
+        }
+
+        Navigation.findNavController(view).navigate(R.id.action_customerServicesFragment_to_serviceProviderListFragment, bundle);
     }
 }
